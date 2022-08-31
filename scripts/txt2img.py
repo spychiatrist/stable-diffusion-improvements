15edcd65aa98825b4bc78242d2cf3b90c0b414c7
Nathan Adara <nathanadara@gmail.com>
2022-08-31 15:33:23 +0200

    Reorganize main file, use modified T2I and KSampler

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -23,318 +23,31 @@ from contextlib import contextmanager, nullcontext
 from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
+from ldm.models.diffusion.ksampler import KSampler
+from ldm.simplet2i import T2I
 
-
-def chunk(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
-
-
-def load_model_from_config(config, ckpt, verbose=False):
-    print(f"Loading model from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cpu")
-    if "global_step" in pl_sd:
-        print(f"Global Step: {pl_sd['global_step']}")
-    sd = pl_sd["state_dict"]
-    model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
-
-    model.cuda()
-    model.eval()
-    return model
 
 opt = None
 
+window = None
+
 def main():
-    global window
     global opt
-    parser = argparse.ArgumentParser()
+    global window
+    opt = parse_args()
+    
 
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        nargs="?",
-        default="a painting of a virus monster playing guitar",
-        help="the prompt to render"
-    )
-    parser.add_argument(
-        "--outdir",
-        type=str,
-        nargs="?",
-        help="dir to write results to",
-        default="outputs/txt2img-samples"
-    )
-    parser.add_argument(
-        "--skip_grid",
-        action='store_true',
-        help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
-    )
-    parser.add_argument(
-        "--skip_save",
-        action='store_true',
-        help="do not save individual samples. For speed measurements.",
-    )
-    parser.add_argument(
-        "--interactive",
-        action='store_true',
-        help="run in interactive mode",
-    )
-    parser.add_argument(
-        "--ddim_steps",
-        type=int,
-        default=40,
-        help="number of ddim sampling steps",
-    )
-    parser.add_argument(
-        "--plms",
-        action='store_true',
-        help="use plms sampling",
-    )
-    parser.add_argument(
-        "--laion400m",
-        action='store_true',
-        help="uses the LAION400M model",
-    )
-    parser.add_argument(
-        "--fixed_code",
-        action='store_true',
-        help="if enabled, uses the same starting code across samples ",
-    )
-    parser.add_argument(
-        "--ddim_eta",
-        type=float,
-        default=0.0,
-        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
-    )
-    parser.add_argument(
-        "--n_iter",
-        type=int,
-        default=1,
-        help="sample this often",
-    )
-    parser.add_argument(
-        "--H",
-        type=int,
-        default=512,
-        help="image height, in pixel space",
-    )
-    parser.add_argument(
-        "--W",
-        type=int,
-        default=512,
-        help="image width, in pixel space",
-    )
-    parser.add_argument(
-        "--C",
-        type=int,
-        default=4,
-        help="latent channels",
-    )
-    parser.add_argument(
-        "--f",
-        type=int,
-        default=8,
-        help="downsampling factor",
-    )
-    parser.add_argument(
-        "--n_samples",
-        type=int,
-        default=1,
-        help="how many samples to produce for each given prompt. A.k.a. batch size",
-    )
-    parser.add_argument(
-        "--n_rows",
-        type=int,
-        default=0,
-        help="rows in the grid (default: n_samples)",
-    )
-    parser.add_argument(
-        "--scale",
-        type=float,
-        default=9.0,
-        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
-    )
-    parser.add_argument(
-        "--from-file",
-        type=str,
-        help="if specified, load prompts from this file",
-    )
-    parser.add_argument(
-        "--config",
-        type=str,
-        default="configs/stable-diffusion/v1-inference.yaml",
-        help="path to config which constructs model",
-    )
-    parser.add_argument(
-        "--ckpt",
-        type=str,
-        default="models/ldm/stable-diffusion-v1/model.ckpt",
-        help="path to checkpoint of model",
-    )
-    parser.add_argument(
-        "--seed",
-        type=int,
-        default=42,
-        help="the seed (for reproducible sampling)",
-    )
-    parser.add_argument(
-        "--seed_offset",
-        type=int,
-        default=0,
-        help="how many samples forward should the seed state emulate",
-    )
-    parser.add_argument(
-        "--precision",
-        type=str,
-        help="evaluate at this precision",
-        choices=["full", "autocast"],
-        default="autocast"
-    )
-    opt = parser.parse_args()
-
-    if opt.laion400m:
-        print("Falling back to LAION 400M model...")
-        opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
-        opt.ckpt = "models/ldm/text2img-large/model.ckpt"
-        opt.outdir = "outputs/txt2img-samples-laion400m"
-
-    if opt.interactive:
-        threading.Thread(target=ui_thread).start()
-        sem_generate.acquire()
-
+    window = make_ui()
     seed_everything(opt.seed)
-    
-    config = OmegaConf.load(f"{opt.config}")
-    model = load_model_from_config(config, f"{opt.ckpt}")
-
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = model.to(device)
-
-    if opt.plms:
-        sampler = PLMSSampler(model)
-    else:
-        sampler = DDIMSampler(model)
 
     
+    sem_generate.acquire()
+    threading.Thread(target=backend_loop).start()
+    ui_thread(window)
 
-    precision_scope = autocast if opt.precision=="autocast" else nullcontext
-    with torch.no_grad():
-        with precision_scope("cuda"):
-            with model.ema_scope():
-
-
-                while True:
-                    #await mutex to proceed
-                    if opt.interactive:
-                        window.write_event_value("-READY-", None)
-                    sem_generate.acquire()
-
-                    os.makedirs(opt.outdir, exist_ok=True)
-                    outpath = opt.outdir
-
-                    batch_size = opt.n_samples
-                    n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
-                    
-
-                    sample_path = os.path.join(outpath, "samples")
-                    os.makedirs(sample_path, exist_ok=True)
-                    base_count = len(os.listdir(sample_path))
-                    grid_count = len(os.listdir(outpath)) - 1
-
-                    start_code = None
-                    if opt.fixed_code:
-                        start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
-
-
-                    if not opt.from_file:
-                        prompt = opt.prompt
-                        assert prompt is not None
-                        data = [batch_size * [prompt]]
-
-                    else:
-                        print(f"reading prompts from {opt.from_file}")
-                        with open(opt.from_file, "r") as f:
-                            data = f.read().splitlines()
-                            n_rows = len(data)
-                            data = list(chunk(data, batch_size))
-
-                    def interactiveCallback( i ):
-                        window.write_event_value('-ITER-', i)
-
-                    tic = time.time()
-                    all_samples = list()
-                    for n in trange(opt.n_iter, desc="Sampling"):
-                        for prompts in tqdm(data, desc="data"):
-
-                            seed_everything(opt.seed)
-                            uc = None
-                            if opt.scale != 1.0:
-                                uc = model.get_learned_conditioning(batch_size * [""])
-                            if isinstance(prompts, tuple):
-                                prompts = list(prompts)
-                            c = model.get_learned_conditioning(prompts)
-                            shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
-                            samples_ddim, _ = sampler.sample(S=opt.ddim_steps,
-                                                            conditioning=c,
-                                                            batch_size=opt.n_samples,
-                                                            shape=shape,
-                                                            verbose=False,
-                                                            unconditional_guidance_scale=opt.scale,
-                                                            unconditional_conditioning=uc,
-                                                            eta=opt.ddim_eta,
-                                                            seed_offset=opt.seed_offset,
-                                                            x_T=start_code,
-                                                            callback=interactiveCallback if opt.interactive else None)
-
-                            x_samples_ddim = model.decode_first_stage(samples_ddim)
-                            x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-
-                            for i, x_sample in enumerate(x_samples_ddim):
-                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                                img = Image.fromarray(x_sample.astype(np.uint8))
-                                if opt.interactive:
-                                    _metadata = (img, opt.__dict__.copy(), i)
-                                    window.write_event_value("-IMAGE-", _metadata)
-                                if not opt.skip_save:
-                                    img.save(os.path.join(sample_path, f"{base_count:05}.png"))
-                                    base_count += 1
-
-                            if not opt.skip_grid:
-                                all_samples.append(x_samples_ddim)
-
-                            opt.seed += 1
-
-                    if not opt.skip_grid:
-                        # additionally, save as grid
-                        grid = torch.stack(all_samples, 0)
-                        grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-                        grid = make_grid(grid, nrow=n_rows)
-
-                        # to image
-                        grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-                        Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
-                        grid_count += 1
-
-                    toc = time.time()
-
-                    if not opt.interactive:
-                        break
-
-
-    print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
-          f" \nEnjoy.")
-
-window: sg.Window = None
 
 def make_ui():
     global opt
-    global window
     print("Starting UI...")
 
     programName = 'Stable Diffusion Interactive'
@@ -406,6 +119,7 @@ def make_ui():
     ]
 
     window = sg.Window(programName, layout, finalize=True)
+    return window
 
 
 sbar_colors = {'l': sg.theme_text_color(), 'w': '#a6943c', 'e': '#b03e3e',}
@@ -414,11 +128,9 @@ sem_generate = threading.Semaphore(1)
 
 curr_sample_i = 0
 
-def ui_thread():
+def ui_thread(window:sg.Window):
     global opt
     global curr_sample_i
-    global window
-    make_ui()
 
     datalist = []
     imgKeys = [
@@ -560,8 +272,9 @@ def ui_thread():
             SetSampleAndInfo(_i)
 
         elif event == '-ITER-':
-
-            window['GenerateProgress'].update(current_count=values[event], max=opt.ddim_steps)
+            _x0, _i = values[event]
+            # TODO: animate substepping
+            window['GenerateProgress'].update(current_count=_i, max=opt.ddim_steps)
             
         elif event == '-SAVE-':
             if len(datalist) > 0:
@@ -647,8 +360,229 @@ def ui_thread():
         if event == sg.WIN_CLOSED:
             os._exit(1)
 
+def backend_loop():
+    global window
+
+    t2i = T2I(
+        width=opt.W,
+        height=opt.H,
+        sampler_name=opt.sampler,
+        weights=opt.ckpt,
+        full_precision=(opt.precision == "full"),
+        config=opt.config,
+        # grid  = opt.grid,
+        # this is solely for recreating the prompt
+        latent_diffusion_weights=opt.laion400m,
+        embedding_path=opt.embedding_path,
+        device=opt.device,
+    )
+
+    t2i.load_model()
+
+    while True:
+        #await mutex to proceed
+        window.write_event_value("-READY-", None)
+        sem_generate.acquire()
+
+        os.makedirs(opt.outdir, exist_ok=True)
+        outpath = opt.outdir
+
+        sample_path = os.path.join(outpath, "samples")
+        os.makedirs(sample_path, exist_ok=True)
+
+
+
+        def substep_callback( x0,  i ):
+            window.write_event_value('-ITER-', (x0, i))
+
+        def iter_callback(img:Image, seed, i):
+            opt.seed=seed
+            _metadata = (img, opt.__dict__.copy(), i)
+            window.write_event_value("-IMAGE-", _metadata)
+            
+
+        seed_everything(opt.seed)
+        t2i.prompt2image(prompt=opt.prompt,
+            iterations=opt.n_iter,
+            batch_size=opt.n_samples,
+            steps=opt.ddim_steps,
+            seed=opt.seed,
+            cfg_scale=opt.scale,
+            ddim_eta=opt.ddim_eta,
+            image_callback=iter_callback,
+            step_callback=substep_callback,
+            width=opt.W,
+            height=opt.H,
+            sampler_name=opt.sampler
+        )
+
 
     
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        nargs="?",
+        default="a painting of a virus monster playing guitar",
+        help="the prompt to render"
+    )
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        nargs="?",
+        help="dir to write results to",
+        default="outputs/txt2img-samples"
+    )
+    parser.add_argument(
+        "--skip_grid",
+        action='store_true',
+        help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
+    )
+    parser.add_argument(
+        "--skip_save",
+        action='store_true',
+        help="do not save individual samples. For speed measurements.",
+    )
+    parser.add_argument(
+        "--interactive",
+        action='store_true',
+        help="run in interactive mode",
+    )
+    parser.add_argument(
+        "--ddim_steps",
+        type=int,
+        default=40,
+        help="number of ddim sampling steps",
+    )
+    parser.add_argument(
+        "--sampler",
+        type=str,
+        help="which sampler to use",
+        default="k_lms"
+    )
+    parser.add_argument(
+        "--laion400m",
+        action='store_true',
+        help="uses the LAION400M model",
+    )
+    parser.add_argument(
+        "--fixed_code",
+        action='store_true',
+        help="if enabled, uses the same starting code across samples ",
+    )
+    parser.add_argument(
+        "--ddim_eta",
+        type=float,
+        default=0.0,
+        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+    )
+    parser.add_argument(
+        "--n_iter",
+        type=int,
+        default=1,
+        help="sample this often",
+    )
+    parser.add_argument(
+        "--H",
+        type=int,
+        default=512,
+        help="image height, in pixel space",
+    )
+    parser.add_argument(
+        "--W",
+        type=int,
+        default=512,
+        help="image width, in pixel space",
+    )
+    parser.add_argument(
+        "--C",
+        type=int,
+        default=4,
+        help="latent channels",
+    )
+    parser.add_argument(
+        "--f",
+        type=int,
+        default=8,
+        help="downsampling factor",
+    )
+    parser.add_argument(
+        "--n_samples",
+        type=int,
+        default=1,
+        help="how many samples to produce for each given prompt. A.k.a. batch size",
+    )
+    parser.add_argument(
+        "--n_rows",
+        type=int,
+        default=0,
+        help="rows in the grid (default: n_samples)",
+    )
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=9.0,
+        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+    )
+    parser.add_argument(
+        "--from-file",
+        type=str,
+        help="if specified, load prompts from this file",
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/stable-diffusion/v1-inference.yaml",
+        help="path to config which constructs model",
+    )
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        default="models/ldm/stable-diffusion-v1/model.ckpt",
+        help="path to checkpoint of model",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="the seed (for reproducible sampling)",
+    )
+    parser.add_argument(
+        "--seed_offset",
+        type=int,
+        default=0,
+        help="how many samples forward should the seed state emulate",
+    )
+    parser.add_argument(
+        "--precision",
+        type=str,
+        help="evaluate at this precision",
+        choices=["full", "autocast"],
+        default="autocast"
+    )
+    parser.add_argument(
+        '--device',
+        '-d',
+        type=str,
+        default='cuda',
+        help='Device to run Stable Diffusion on. Defaults to cuda `torch.cuda.current_device()` if avalible',
+    )
+    parser.add_argument(
+        '--embedding_path',
+        type=str,
+        help='Path to a pre-trained embedding manager checkpoint - can only be set on command line',
+    )
+    opt = parser.parse_args()
+
+    if opt.laion400m:
+        print("Falling back to LAION 400M model...")
+        opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
+        opt.ckpt = "models/ldm/text2img-large/model.ckpt"
+        opt.outdir = "outputs/txt2img-samples-laion400m"
+
+    return opt
 
 
 
